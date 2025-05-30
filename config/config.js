0bfb059f18d665f3ad4f363abe41d1e28ee6df2b
loganathanramsaran <loguflower3@gmail.com>
2025-05-31 00:12:15 +0530

    env err fixed

--- config/config.js
+++ config/config.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 
-const config = {
-  port: process.env.PORT || 6000, 
-  mongoURI: process.env.MONGO_URI, 
-  jwtSecret: process.env.JWT_SECRET, 
-}
-
-module.exports = config;
+module.exports = {
+  port: process.env.PORT || 6000,
+  mongoURI: process.env.MONGO_URI,
+  jwtSecret: process.env.JWT_SECRET,
+};
 
