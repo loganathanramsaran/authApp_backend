3202c6af9a652c6056ef7483d7c5e0b523d8316b
loganathanramsaran <loguflower3@gmail.com>
2025-05-30 22:36:29 +0530

    connection err

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ const config = require('./config');
 
 const connectDB = async () => {
   try {
-    const connectDB = await mongoose.connect(config.mongoURI);
     console.log(`MongoDB Connected successfully`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
