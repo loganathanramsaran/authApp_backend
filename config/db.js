d9e3af82e20ab51a35bb1167e3d807d03b95ab32
loganathanramsaran <loguflower3@gmail.com>
2025-05-31 00:27:02 +0530

    test

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 const config = require('./config');
 
 const connectDB = async () => {
   try {
+    console.log('Mongo URI:', config.mongoURI); // Debug log
     await mongoose.connect(config.mongoURI);
     console.log('MongoDB Connected successfully');
   } catch (error) {
